d0f4689fbdd09fa8b414818dcbf104d85a72cee2
alexwaitz <alexanderjwaitz@gmail.com>
2018-01-06 18:08:53 -0800

    updated build script

--- build.sh
+++ build.sh
@@ -7,15 +7,19 @@ mkdir releases/macos
 mkdir releases/win
 mkdir releases/linux
 
-mv releases/wikisets-macos releases/macos
-mv releases/wikisets-win.exe releases/win
-mv releases/wikisets-linux releases/linux
+mv releases/wikisets-macos releases/macos/wikisets
+mv releases/wikisets-win.exe releases/win/wikisets.exe
+mv releases/wikisets-linux releases/linux/wikisets
 
 cp pkg-src/install.sh releases/macos/
 cp pkg-src/uninstall.sh releases/macos/
 
 cp pkg-src/install.sh releases/linux/
 cp pkg-src/uninstall.sh releases/linux/
+
+cp LICENSE releases/macos/
+cp LICENSE releases/win/
+cp LICENSE releases/linux/
 
 zip -r releases/macos.zip releases/macos/
 zip -r releases/win.zip releases/win/
